363e79eb687e7faf2f7c00863a36f799c87f86a1
jhonadc <jhonathanaugusto@gmail.com>
2022-04-04 20:35:20 +0200

    App created - FIRESTORE config added

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h2>My app</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXMVJxOmwnOwhvV-CabhAkQkS8PtdYpe4',
+  authDomain: 'house-marketplace-app-e66ef.firebaseapp.com',
+  projectId: 'house-marketplace-app-e66ef',
+  storageBucket: 'house-marketplace-app-e66ef.appspot.com',
+  messagingSenderId: '804691766591',
+  appId: '1:804691766591:web:1efba6c1b1b6cb51f3778a',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+export const db = getfirestore();
