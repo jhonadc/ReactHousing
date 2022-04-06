be0be235e7fd522ddfb101ae76a85500b01e86ad
jhonadc <jhonathanaugusto@gmail.com>
2022-04-06 16:31:40 +0200

    toastify installed - error message on sign in and up configured

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/NavBar';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
@@ -8,7 +10,6 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
-
   return (
     <>
       <Router>
@@ -22,6 +23,7 @@ function App() {
         </Routes>
         <Navbar />
       </Router>
+      <ToastContainer />
     </>
   );
 }
