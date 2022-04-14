c7bd6f9e2527b711640d04640d60dc1d74f7cccf
jhonadc <jhonathanaugusto@gmail.com>
2022-04-14 12:53:19 +0200

    privateroute component and useAuthStatus to allow profile page access only to loggedIn users

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStatus } from '../hooks/useAuthStatus';
+
+const PrivateRoute = () => {
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) {
+    return <h3>Loading...</h3>;
+  }
+  return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
+};
+
+export default PrivateRoute;
+
+// https://stackoverflow.com/questions/65505665/protected-route-with-firebase
